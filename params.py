8afe23da334258febd25bfa40c4409930b3c61c7
Margoo9 <39960184+Margoo9@users.noreply.github.com>
2024-09-17 15:19:47 +0200

    Update params.py

--- params.py
+++ params.py
@@ -1,11 +1,10 @@
 import numpy as np
 
 
-number_of_points = 128
-
 # pendulum params
 init_point_pendulum = [0.75 * np.pi, 0.0]
-t_pend = np.linspace(0, 10, number_of_points)
+number_of_points_pendulum = 100
+t_pend = np.linspace(0, 15, number_of_points_pendulum)
 b = 0.3    # damping coefficient -> effect of friction
 c = 4.0     # const, depends on the length and mass of the pendulum
 # b*omega - damping or frictional force
@@ -13,8 +12,9 @@ c = 4.0     # const, depends on the length and mass of the pendulum
 
 # lotka-volterra params
 init_point_lv = [15, 7]
-t_lv = np.linspace(0, 100, number_of_points)
-alpha = 1.5
-beta = 1.2
-gamma = 1.3
-delta = 0.9
+number_of_points_lv = 100
+t_lv = np.linspace(0, 32, number_of_points_lv)
+alpha = 0.9
+beta = 0.5
+gamma = 0.75
+delta = 0.25
