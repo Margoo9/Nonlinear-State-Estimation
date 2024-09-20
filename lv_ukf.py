7846bb050ee1fc8a846265cb9cfb0b9edf9cb912
Margoo9 <39960184+Margoo9@users.noreply.github.com>
2024-09-20 22:01:47 +0200

    Update lv_ukf.py

--- lv_ukf.py
+++ lv_ukf.py
@@ -11,8 +11,10 @@ import time
 
 # np.random.seed(42)
 
-def lv_ukf():
-    def sqrt_func(x):
+
+def lv_ukf(observed_lv, true_state_lv, lv_equations, lv_solved, t_lv, theta_lv, init_point_lv):
+    alpha, beta, gamma, delta = theta_lv
+    def cholesky_func(x):
         try:
             result = scipy.linalg.cholesky(x)
         except scipy.linalg.LinAlgError:
@@ -26,7 +28,8 @@ def lv_ukf():
     start_time = time.time()
 
     def f_cv(x, dt):
-        sol = odeint(lv_equations, x, [0, dt], args=(alpha, beta, gamma, delta))
+        sol = odeint(lv_equations, x, [0, dt], args=(theta_lv,))
+        # sol = odeint(lv_equations, y0=init_point_lv, t=t_lv, rtol=0.01, args=(theta_lv,))
         return sol[-1]
 
     def h_cv(x):
@@ -34,8 +37,8 @@ def lv_ukf():
 
     measurement_noise_std = 1
 
-    # points = MerweScaledSigmaPoints(2, alpha=1e-3, beta=2., kappa=1.0, sqrt_method=sqrt_func)
-    points = MerweScaledSigmaPoints(2, alpha=0.1, beta=2., kappa=0.0, sqrt_method=sqrt_func)
+    # points = MerweScaledSigmaPoints(2, alpha=1e-3, beta=2., kappa=1.0, sqrt_method=cholesky_func)
+    points = MerweScaledSigmaPoints(2, alpha=0.1, beta=2., kappa=0.0, sqrt_method=cholesky_func)
     ukf = UKF(dim_x=2, dim_z=2, fx=f_cv, hx=h_cv, dt=0.1, points=points)
     ukf.P = np.diag([10, 10])  # Initialize with larger values
     ukf.x = np.array(init_point_lv)
@@ -59,16 +62,11 @@ def lv_ukf():
 
     predictions = list(zip(kf_theta_values, kf_omega_values))
 
-
-
     sampling_time = time.time() - start_time
 
-    # print(sol)
-
-    rms = mean_squared_error(sol, predictions, squared=False)
-    print(f'RMSE of pendulum predictions using SMC: {rms}')
-    print(f'Sampling time of pendulum using SMC is: {sampling_time}')
-
+    rms = mean_squared_error(true_state_lv, predictions, squared=False)
+    print(f'RMSE of lv predictions using UKF: {rms}')
+    print(f'Sampling time of lv using UKF is: {sampling_time}')
 
     fig, (ax1, ax2) = plt.subplots(2, figsize=(12, 8))
     ax1.plot(observed_lv[:, 0], 'x', label='Pomiary')
@@ -78,7 +76,7 @@ def lv_ukf():
     ax1.set_ylabel('Liczba ofiar')
 
     ax2.plot(observed_lv[:, 1], 'x', label='Pomiary')
-    ax2.plot(true_state[:, 1], 'g', label='Modelowe wartości')
+    ax2.plot(true_state_lv[:, 1], 'g', label='Modelowe wartości')
     ax2.plot(kf_omega_values, label='Estymata - algorytm UKF')
 
     plt.xlabel('Czas')
@@ -87,7 +85,4 @@ def lv_ukf():
     plt.savefig('obj2_ukf_predictions.pdf')
 
     # plt.show()
-
-
-if __name__ == '__main__':
-    lv_ukf()
+    return predictions, sampling_time
