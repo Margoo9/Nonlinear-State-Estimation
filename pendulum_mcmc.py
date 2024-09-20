424c82e74b0d35f3f6d348fa26fe4834d6647562
Margoo9 <39960184+Margoo9@users.noreply.github.com>
2024-09-20 22:03:51 +0200

    Update pendulum_mcmc.py

--- pendulum_mcmc.py
+++ pendulum_mcmc.py
@@ -13,7 +13,7 @@ import time
 # np.random.seed(42)
 
 
-def slice():
+def pend_slice(observed_pend, true_state_pend, pendulum_equations, pendulum_solved, t_pend, theta_pend, init_point_pendulum):
     start_time = time.time()
 
     @as_op(itypes=[pt.dvector], otypes=[pt.dmatrix])
@@ -29,10 +29,10 @@ def slice():
             pm.math.stack([b, c])
         )
 
-        pm.Normal("Y_obs", mu=ode_solution, sigma=sigma, observed=data)
+        pm.Normal("Y_obs", mu=ode_solution, sigma=sigma, observed=observed_pend)
 
     pm.model_to_graphviz(model=model)
-    plt.show()
+    # plt.show()
 
     with model:
         trace_slice = pm.sample(step=[pm.Slice()], tune=2000, draws=2000)
@@ -71,15 +71,16 @@ def slice():
     print(f'RMSE of pendulum predictions using Slice: {rms}')
     print(f'Sampling time of pendulum using slice is: {sampling_time}')
 
-    return trace_slice, observed_pend, predictions, rms, true_state_pend
+    # return trace_slice, observed_pend, predictions, rms, true_state_pend
+    return predictions, sampling_time
 
 
-def metropolis():
+def pend_metropolis(observed_pend, true_state_pend, pendulum_equations, pendulum_solved, t_pend, theta_pend, init_point_pendulum):
     start_time = time.time()
 
     @as_op(itypes=[pt.dvector], otypes=[pt.dmatrix])
     def pytensor_forward_model_matrix(theta_pend):
-        return odeint(func=pend_equations, y0=init_point_pendulum, t=t_pend, rtol=0.01, args=(theta_pend,))
+        return odeint(func=pendulum_equations, y0=init_point_pendulum, t=t_pend, rtol=0.01, args=(theta_pend,))
 
     with pm.Model() as model:
         b = pm.Normal("b")
@@ -93,21 +94,21 @@ def metropolis():
         pm.Normal("Y_obs", mu=ode_solution, sigma=sigma, observed=observed_pend)
 
     pm.model_to_graphviz(model=model)
-    plt.show()
+    # plt.show()
 
     with model:
-        trace_slice = pm.sample(step=[pm.Metropolis()], tune=2000, draws=2000)
+        trace_metropolis = pm.sample(step=[pm.Metropolis()], tune=2000, draws=2000)
 
     sampling_time = time.time() - start_time
 
-    az.plot_trace(trace_slice)
+    az.plot_trace(trace_metropolis)
     plt.savefig('obj1_trace_metropolis.pdf')
     # plt.show()
-    az.plot_posterior(trace_slice)
+    az.plot_posterior(trace_metropolis)
     plt.savefig('obj1_posterior_metropolis.pdf')
     # plt.show()
 
-    posterior = trace_slice.posterior.stack(samples=("draw", "chain"))
+    posterior = trace_metropolis.posterior.stack(samples=("draw", "chain"))
     theta_posterior = [posterior["b"].mean(), posterior["c"].mean()]
     predictions = pendulum_solved(None, theta_posterior)
     fig, (ax1, ax2) = plt.subplots(2, figsize=(12, 8))
@@ -117,7 +118,7 @@ def metropolis():
     ax1.legend(loc='upper right')
     ax1.set_ylabel('Kąt odchylenia')
 
-    ax2.plot(served_pend[:, 1], 'x', label='Pomiary')
+    ax2.plot(observed_pend[:, 1], 'x', label='Pomiary')
     ax2.plot(true_state_pend[:, 1], 'g', label='Modelowe wartości')
     ax2.plot(predictions[:, 1], label='Estymata - algorytm Metropolisa')
 
@@ -132,10 +133,11 @@ def metropolis():
     print(f'RMSE of pendulum predictions using Metropolis: {rms}')
     print(f'Sampling time of pendulum using Metropolis is: {sampling_time}')
 
-    return trace_slice, observed_pend, predictions, rms, true_state_pend
+    # return trace_slice, observed_pend, predictions, rms, true_state_pend
+    return predictions, sampling_time
 
 
-def nuts():
+def pend_nuts(observed_pend, true_state_pend, pendulum_equations, pendulum_solved, t_pend, theta_pend, init_point_pendulum):
     ode_model = DifferentialEquation(
         func=pendulum_equations, times=t_pend, n_states=2, n_theta=2, t0=0
     )
@@ -152,27 +154,27 @@ def nuts():
         c = pm.Normal("c")
         sigma = pm.HalfNormal("sigma", 10)
 
-        ode_solution = ode_model(y0=X0, theta=[b, c])
+        ode_solution = ode_model(y0=init_point_pendulum, theta=[b, c])
 
         pm.Normal("Y_obs", mu=ode_solution, sigma=sigma, observed=observed_pend)
 
     pm.model_to_graphviz(model=model)
-    plt.show()
+    # plt.show()
 
     with model:
         step = pm.NUTS()
-        trace_slice = pm.sample(step=step, tune=2000, draws=2000)
+        trace_nuts = pm.sample(step=step, tune=2000, draws=2000)
 
     sampling_time = time.time() - start_time
 
-    az.plot_trace(trace_slice)
+    az.plot_trace(trace_nuts)
     plt.savefig('obj1_trace_nuts.pdf')
     # plt.show()
-    az.plot_posterior(trace_slice)
+    az.plot_posterior(trace_nuts)
     plt.savefig('obj1_posterior_nuts.pdf')
     # plt.show()
 
-    posterior = trace_slice.posterior.stack(samples=("draw", "chain"))
+    posterior = trace_nuts.posterior.stack(samples=("draw", "chain"))
     theta_posterior = [posterior["b"].mean(), posterior["c"].mean()]
     predictions = pendulum_solved(None, theta_posterior)
     fig, (ax1, ax2) = plt.subplots(2, figsize=(12, 8))
@@ -197,111 +199,5 @@ def nuts():
     print(f'RMSE of pendulum predictions using NUTS: {rms}')
     print(f'Sampling time of pendulum using NUTS is: {sampling_time}')
 
-    return trace_slice, observed_pend, predictions, rms, true_state_pend
-
-
-def run_simulation_slice(_):
-    return slice()
-
-
-def run_simulation_metropolis(_):
-    return metropolis()
-
-
-def run_simulation_nuts(_):
-    return nuts()
-
-
-if __name__ == '__main__':
-    freeze_support()
-    num_runs = 10
-
-    with Pool() as pool:
-        results_slice = pool.map(run_simulation_slice, range(num_runs))
-
-    all_traces_slice, all_data_slice, all_predicitons_slice, all_rmse_slice, x_y_slice = zip(*results_slice)
-
-    combined_trace_slice = az.concat(all_traces_slice)
-
-    average_data_slice = np.mean(all_data_slice, axis=0)
-    average_predictions_slice = np.mean(all_predicitons_slice, axis=0)
-    average_rmse_slice = np.mean(all_rmse_slice)
-
-    fig, (ax1, ax2) = plt.subplots(2, figsize=(12, 8))
-    ax1.plot(average_data_slice[:, 0], 'x', label='Pomiary')
-    ax2.plot(x_y_slice[:, 1], 'g', label='Modelowe wartości')
-    ax1.plot(average_predictions_slice[:, 0], label='Estymata - próbkowanie przekrojów')
-    ax1.legend(loc='upper right')
-    ax1.set_ylabel('Kąt odchylenia')
-
-    ax2.plot(average_data_slice[:, 1], 'x', label='Pomiary')
-    ax2.plot(x_y_slice[:, 1], 'g', label='Modelowe wartości')
-    ax2.plot(average_predictions_slice[:, 1], label='Estymata - próbkowanie przekrojów')
-
-    plt.xlabel('Czas')
-    ax2.legend(loc='upper right')
-    ax2.set_ylabel('Prędkość kątowa')
-    plt.savefig('average_slice_predictions.pdf')
-
-    print(f'Average RMSE of pendulum predictions using Slice: {average_rmse_slice}')
-
-    with Pool() as pool:
-        results_metropolis = pool.map(run_simulation_metropolis, range(num_runs))
-
-    all_traces_metropolis, all_data_metropolis, all_predicitons_metropolis, all_rmse_metropolis, x_y_metropolis = zip(*results_metropolis)
-
-    combined_trace_metropolis = az.concat(all_traces_metropolis)
-
-    average_data_metropolis = np.mean(all_data_metropolis, axis=0)
-    average_predictions_metropolis = np.mean(all_predicitons_metropolis, axis=0)
-    average_rmse_metropolis = np.mean(all_rmse_metropolis)
-
-    fig, (ax1, ax2) = plt.subplots(2, figsize=(12, 8))
-    ax1.plot(average_data_metropolis[:, 0], 'x', label='Pomiary')
-    ax2.plot(x_y_metropolis[:, 1], 'g', label='Modelowe wartości')
-    ax1.plot(average_predictions_metropolis[:, 0], label='Estymata - algorytm Metropolisa')
-    ax1.legend(loc='upper right')
-    ax1.set_ylabel('Kąt odchylenia')
-
-    ax2.plot(average_data_metropolis[:, 1], 'x', label='Pomiary')
-    ax2.plot(x_y_metropolis[:, 1], 'g', label='Modelowe wartości')
-    ax2.plot(average_predictions_metropolis[:, 1], label='Estymata - algorytm Metropolisa')
-
-    plt.xlabel('Czas')
-    ax2.legend(loc='upper right')
-    ax2.set_ylabel('Prędkość kątowa')
-    plt.savefig('average_metropolis_predictions.pdf')
-
-    print(f'Average RMSE of pendulum predictions using metropolis: {average_rmse_metropolis}')
-
-
-    with Pool() as pool:
-        results_nuts = pool.map(run_simulation_nuts, range(num_runs))
-
-    all_traces_nuts, all_data_nuts, all_predicitons_nuts, all_rmse_nuts, x_y_nuts = zip(*results_nuts)
-
-    combined_trace_nuts = az.concat(all_traces_nuts)
-
-    average_data_nuts = np.mean(all_data_nuts, axis=0)
-    average_predictions_nuts = np.mean(all_predicitons_nuts, axis=0)
-    average_rmse_nuts = np.mean(all_rmse_nuts)
-
-    fig, (ax1, ax2) = plt.subplots(2, figsize=(12, 8))
-    ax1.plot(average_data_nuts[:, 0], 'x', label='Pomiary')
-    ax2.plot(x_y_nuts[:, 1], 'g', label='Modelowe wartości')
-    ax1.plot(average_predictions_nuts[:, 0], label='Estymata - algorytm NUTS')
-    ax1.legend(loc='upper right')
-    ax1.set_ylabel('Kąt odchylenia')
-
-    ax2.plot(average_data_nuts[:, 1], 'x', label='Pomiary')
-    ax2.plot(x_y_nuts[:, 1], 'g', label='Modelowe wartości')
-    ax2.plot(average_predictions_nuts[:, 1], label='Estymata - algorytm NUTS')
-
-    plt.xlabel('Czas')
-    ax2.legend(loc='upper right')
-    ax2.set_ylabel('Prędkość kątowa')
-    plt.savefig('average_nuts_predictions.pdf')
-
-    print(f'Average RMSE of pendulum predictions using nuts: {average_rmse_nuts}')
-    plt.show()
-    
+    # return trace_slice, observed_pend, predictions, rms, true_state_pend
+    return predictions, sampling_time
